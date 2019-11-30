381dd452a180bd25e4b2b2cecd44ee3e02a7f645
Peter-cloud-web <petergichia165@gmail.com>
2019-11-30 21:42:10 +0300

    Use if and else if

--- Akan.js
+++ Akan.js
@@ -10,6 +10,10 @@ function akan(){
     var Day =  Math.ceil((((((CC/4) - 2 * CC - 1) + ((5 * YY/4)) + ((26 * MM + 1)/10)) + DD ) % 7));
     alert( "You were born on " +Day + "of the week"); 
 
+    if (Operators = "Male" && Day == 0) {
+    alert("You were born on Sunday so your Akan name is Kwasi");
+}
+
 
    
 }
