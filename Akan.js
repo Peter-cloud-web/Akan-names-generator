cbdf9efd2f4e6a4d7d3988f267877240e8ab67aa
Peter-cloud-web <petergichia165@gmail.com>
2019-11-30 21:09:46 +0300

    Convert strings to integers using parse

--- Akan.js
+++ Akan.js
@@ -1,3 +1,10 @@
 function akan(){
-	
+	var dateofbirth = document.getElementById("dateofbirth").value;
+    var DD = parseInt(dateofbirth);
+    var month = document.getElementById("month").value;
+    var MM = parseInt(month);
+    var year = document.getElementById("year").value;
+    var YY = parseInt(year.slice(2)); 
+    var CC = parseInt(year.slice(0,2));
+
 }
